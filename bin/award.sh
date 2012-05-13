c397cf34d6c73c592ab02f02564a3446db312a46
stef <stefan.marsiske@gmail.com>
2012-05-14 01:00:00 +0200

    [enh] added OTR trust challenge

--- bin/award.sh
+++ bin/award.sh
@@ -2,4 +2,4 @@
 
 basedir=$(realpath "${0%/*}/..")
 alias gpg='gpg --keyring $basedir/keys/keyring.pub --no-default-keyring --secret-keyring $basedir/keys/keyring.sec'
-(echo "Achievement unlocked: $1";date -u) | gpg --clearsign --armor --default-key pc@ctrlc.hu 2>/dev/null
+(printf "Achievement unlocked: $1\n";date -u) | gpg --clearsign --armor --default-key pc@ctrlc.hu 2>/dev/null
