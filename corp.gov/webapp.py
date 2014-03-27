b3831cde6059ef9f47f9e79a8655ec7e6c8e35cc
niko io <niko@niko.io>
2014-03-27 16:15:44 +0100

    added corp.gov

--- corp.gov/webapp.py
+++ corp.gov/webapp.py
@@ -51,7 +51,11 @@ def contex():
 @app.route('/')
 def index():
   return render_template('base.html')
-  
+
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template('404.html'), 404
+
 @app.route('/signup', methods=['GET'])
 def signup():
     t1 = Email()
